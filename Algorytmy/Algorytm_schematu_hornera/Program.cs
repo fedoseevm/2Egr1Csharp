6f10dd7c6f9c5ef6cf073dfcebd30b5f37ff048a
fedoseevm <maksim.fedoseev122@gmail.com>
2024-02-26 14:19:38 +0100

    Directory change

---
+++ Algorytmy/Algorytm_schematu_hornera/Program.cs
@@ -0,0 +1,50 @@
+﻿// Algorytm schematu Hormera
+// Wejscie: system, liczba
+// Wyjscie: liczba w systemie dziesietnym
+
+int horner(int system_licby, string liczba)
+{
+    int wynik = (int)char.GetNumericValue(liczba[0]);
+    for (int i = 1; i < liczba.Length; i++)
+    {
+        wynik = system_licby * wynik + (int)char.GetNumericValue(liczba[i]);
+    }
+    return wynik;
+}
+
+int system_liczby = int.Parse(Console.ReadLine());
+string liczba = Console.ReadLine();
+Console.WriteLine(horner(system_liczby, liczba));
+
+
+// konwersja bin -> dec
+double bin2dec(string bin)
+{
+    double wynik = 0;
+    for (int i = bin.Length - 1; i >= 0; i--)
+    {
+        wynik += Math.Pow(2, bin.Length - i - 1) * (int)char.GetNumericValue(bin[i]);
+    }
+    return wynik;
+}
+string bin = Console.ReadLine();
+Console.WriteLine(bin2dec(bin));
+
+
+// konwersja dec -> bin
+string dec2bin(int dec)
+{
+    string wynik = "";
+    string reszta;
+    while (dec > 0)
+    {
+        reszta = Convert.ToString(dec % 2);
+        wynik += new string(reszta);
+        dec /= 2;
+    }
+    return wynik;
+}
+
+
+int dec = int.Parse(Console.ReadLine());
+Console.WriteLine(dec2bin(dec));
---
+++ Kalkomator project/kalkomator/Program.cs
@@ -0,0 +1,42 @@
+﻿string[] parsujBaze()
+{
+    string[] strings = new string[1];
+    string danePlik = "";
+    using (StreamReader sr = new StreamReader(@"..\..\..\..\baza.txt"))
+        while (!sr.EndOfStream)
+        {
+            danePlik = sr.ReadLine();
+        }
+    if (danePlik != null)
+        return danePlik.Split(',');
+    else
+        return new string[] { "nie" };
+}
+foreach (string s in parsujBaze())
+    Console.WriteLine(s);
+
+
+bool logowanie(string[] data)
+{
+    Console.Write("Podaj login: ");
+    string login = Console.ReadLine();
+    Console.Write("Podaj haslo: ");
+    string haslo = Console.ReadLine();
+    if (login == data[0] && haslo == data[1])
+    {
+        Console.WriteLine("Gotowy na test trzezwosci? [Y/N]: ");    // ???????????????
+        if (Console.ReadKey().Key == ConsoleKey.Y)
+            return true;
+        else
+            return false;
+    }
+
+    else
+    {
+        Console.WriteLine("nieprawidlowe login lub haslo");
+        return false;
+    }
+}
+
+
+Console.WriteLine(logowanie(parsujBaze()));
