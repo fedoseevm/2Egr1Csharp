004044f100ca1e5ec4e122c277b6d07e18464c4c
fedoseevm <maksim.fedoseev122@gmail.com>
2024-03-06 10:55:13 +0100

    Drobne zmiany

--- Algorytmy/Algorytm_schematu_hornera/Program.cs
+++ Algorytmy/Algorytm_schematu_hornera/Program.cs
@@ -15,36 +15,3 @@ int horner(int system_licby, string liczba)
 int system_liczby = int.Parse(Console.ReadLine());
 string liczba = Console.ReadLine();
 Console.WriteLine(horner(system_liczby, liczba));
-
-
-// konwersja bin -> dec
-double bin2dec(string bin)
-{
-    double wynik = 0;
-    for (int i = bin.Length - 1; i >= 0; i--)
-    {
-        wynik += Math.Pow(2, bin.Length - i - 1) * (int)char.GetNumericValue(bin[i]);
-    }
-    return wynik;
-}
-string bin = Console.ReadLine();
-Console.WriteLine(bin2dec(bin));
-
-
-// konwersja dec -> bin
-string dec2bin(int dec)
-{
-    string wynik = "";
-    string reszta;
-    while (dec > 0)
-    {
-        reszta = Convert.ToString(dec % 2);
-        wynik += new string(reszta);
-        dec /= 2;
-    }
-    return wynik;
-}
-
-
-int dec = int.Parse(Console.ReadLine());
-Console.WriteLine(dec2bin(dec));
---
+++ Algorytmy/Konwersja_bin-dec/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Konwersja_bin_dec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // konwersja bin -> dec
+            double bin2dec(string bin)
+            {
+                double wynik = 0;
+                for (int i = bin.Length - 1; i >= 0; i--)
+                {
+                    wynik += Math.Pow(2, bin.Length - i - 1) * (int)char.GetNumericValue(bin[i]);
+                }
+                return wynik;
+            }
+            string bin = Console.ReadLine();
+            Console.WriteLine(bin2dec(bin));
+
+
+            // konwersja dec -> bin
+            string dec2bin(int dec)
+            {
+                string wynik = "";
+                string reszta;
+                while (dec > 0)
+                {
+                    reszta = Convert.ToString(dec % 2);
+                    wynik += new string(reszta);
+                    dec /= 2;
+                }
+                return wynik;
+            }
+            int dec = int.Parse(Console.ReadLine());
+            Console.WriteLine(dec2bin(dec));
+        }
+    }
+}
