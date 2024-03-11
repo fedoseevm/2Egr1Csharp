5ad0d50a21fa3fe07a56236491a19c542dbbfb13
fedoseevm <maksim.fedoseev122@gmail.com>
2024-03-11 14:58:15 +0100

    Add Testy

---
+++ Klasy/Klasy_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasy_1
+{
+    class K1
+    {
+        // Pole
+        protected int Weight;  // Pole z modyfikatorem dostepu protected jest dostepna tylko w tej funkcji i funkcjach dziedziczonych (K2 : K1)
+        private string Name;
+        private int Age;
+        private int Height;
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public int GetHeight()
+        {
+            return Height;
+        }
+    }
+
+    class Zamowienia    // Fasada 
+    {
+        private Klient Klient;
+        private List<Produkt> Produkt;
+        private Dostawa Dostawa;
+
+        public void SetKlient(Klient klient)    // Agregacja
+        {
+            this.Klient = klient;
+        }
+        public void SetProdukt(Produkt produkt) // Agregacja
+        {
+            this.Produkt.Add(produkt);
+        }
+        public void DodajDostawe(Dostawa dostawa)   // Asocjacja
+        {
+            this.Dostawa = dostawa;
+        }
+    }
+    class Klient { }
+    class Produkt { }
+    class Dostawa { }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Testy/Kolekcje/Kolekcje_przygotowanie/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Kolekcje_przygotowanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zadanie 1
+            // Wpisz do tablicy n liczb podanych przez usera w sposob rosnacy i wypisz te z nich, ktore sa "portugalskie"
+            // Liczby portugalskie - liczby samotne czyli takie ktore nie maja sasiada w otoczeniu 2.
+            // WE: 5
+            // 3 7 9 13 19
+            // WY: 3 13 19
+            void zadanie1()
+            {
+                Console.WriteLine("Zadanie 1");
+                Console.Write("Ilosc liczb: ");
+                int n = int.Parse(Console.ReadLine());
+                int[] Insert = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Insert[i] = int.Parse(Console.ReadLine());
+                }
+                int[] Table1 = new int[n];
+                int k = 0;
+                for (int i = Insert.Min(); i <= Insert.Max(); i++) // Wpisywanie podanych liczb w sposob rosnacy do Table1
+                {
+                    if (Insert.Contains(i))
+                    {
+                        Table1[k] = i;
+                        k++;
+                    }
+                }
+                foreach (int i in Table1)
+                    Console.Write(i + " ");
+            }
+            zadanie1();
+
+
+            // Zadanie 2
+            // User podaje dowolny werset z Pana Tadeusza. Sprawdź czy w tym wersecie jakaś literka występuje co najmniej 3 razy. Użyj tablic(toCharArray())
+
+
+            // Zadanie 3
+            // User podaje dwie liczby brytyjskie. Przez liczby brytyjskie rozumiemy dwie dowolne liczby dwucyfrowe różniące się przynajmniej o 50.
+            //Zapisz do listy wszystkie liczby pierwsze znajdujące się między powyższymi liczbami.
+
+
+            // Zadanie 4
+            // Utwórz strukturę losowych osób z losowymi danymi. Zadady:
+            // -wiek między 20 a 50 lat
+            // - imię 4 literowe zaczynająca się na A.
+            // - Nazwisko to odwrócenie imienia.
+            // Wylosuj takie 4 osoby i dodaj je do listy Osób. User nic tu nie wprowadza.
+
+            // Zadanie 5
+            // Utwórz kolejkę liczb doskonałych 
+            // (liczby które równają się sumie swoich dzielników właściwych). 
+            // Umieść w niej 4 najmniejszych liczb doskonałych.
+            // Wyświtl kolejkę, usuń z niej 2 liczby i znów wyświtl to co zostało.
+
+            // Zadanie 6
+            // Stwórz stos nieparzystych dwucyfrowych wieloktorności liczby 13.
+            // Usuń z tego stosu 2 liczby i wyświetl znów ten stos dziwiąc się temu co na nim zostało.
+
+            // Zadanie 7
+            // Kod Hufmanna to zamiana ciągu w stylu AAAAABBBBBBBCCC na 5A7B3C.
+            // Niech user poda wam n ciągów które zapiszecie to zwykłej tablicy.
+            //Potem przenieście te dane do słowinika, gdzie kluczem będzie kod ciągu a wartością ten ciąg.
+
+            // Zadanie 8
+            // Stwórz graf składający się z 6 wierzchołków i 7 krawędzi. 
+            // Wypisz najwyższy stopień wierzchołka. Jeśli kilka wierzchołków ma ten sam stopień to wypisz dowolny z nich.
+
+        }
+    }
+}
